e9d77fe517186d956906974b93eae6311d4a7583
locharp <lochardpatrick@outlook.com>
2022-09-21 17:06:00 +0300

    c

---
+++ C/reverse_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *strrev (char *string)
+{
+  int endIdx = strlen(string) - 1;
+  char s[endIdx + 1];
+
+  for (int i = 0; i <= endIdx; i++)
+    s[i] = string[endIdx - i];
+
+  string = malloc(sizeof(char) * (endIdx + 2));
+  strcpy(string, s);
+
+  return string;
+}
+
+void reverse_string(char str[]) {
+  for (int i = 0, end = strlen(str) - 1; i <= end / 2; i++) {
+    char ch = str[i];
+    str[i] = str[end - i];
+    str[end - i] = ch;
+  }
+}
+
+int main() {
+  char *s = "Hello, world!";
+  printf("%s\n", strrev(s));
+}
+
