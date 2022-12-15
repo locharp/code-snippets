e7c6479b34b47b6172fb03c3777e4667be52064a
locharp <lochardpatrick@outlook.com>
2022-12-15 12:57:41 +0200

    bj

--- Python/bj.py
+++ Python/bj.py
@@ -1,53 +1,38 @@
 from random import randint as rd
 
 n=('Banker','You')
-hs=('Banker has','You have')
+s=('Banker has','You have')
 f=['J','Q','K']
-vs=['A',2,3,4,5,6,7,8,9,10]+f
 
 def t():
- if len(h[p])==2 and(h[p][0]=='A'and h[p][1]in f or h[p][0]in f and h[p][1]=='A'):
-  return -1
- v,a=0,0
+ if len(h[p])==2 and'A'in h[p][:2]and(h[p][0]in f or h[p][1]in f):
+  return 31
+ t,a=0,0
  for c in h[p]:
   if c=='A':
-   v+=1;a=1
-  elif c in f:
-   v+=10
+   t+=10
   else:
-   v+=int(c)
- if v<12 and a:
-  v+=10
- return v
+   t+=int(c)
+ if t<12 and a:
+  t+=10
+ return t if t<22 else -1
 
+def r():
+ h[p]+=[c:=d.pop(rd(0,len(d)-1))]
+ print(f"{n[p]} got {'an'if c=='A'else'a'} {c}.")
+ 
 def m():
- h[p].append(d.pop(rd(0,len(d)-1)))
- print(f'{n[p]} got a {h[p][-1]}.')
- if t()==-1:
-  print(hs[p],'Black Jack!')
- elif t()>21:
-  print(n[p],'busted')
- else:
-  print(f'{hs[p]} {t()}.')
+ r()
+ print(f'{s[p]} Black Jack!'if t()>30 else f'{n[p]} busted!'if t()<0 else f'{s[p]} {t()}.')
 
-while input('\nNew game?(y/n)').lower()=='y':
- d=vs*4;h=[[],[]]
- p=0
- m()
- p=1
- h[1].append(d.pop(rd(0,len(d)-1)))
- print(f'{n[p]} got a {h[p][-1]}.')
- m()
- while t()in range(21)and input('More?(y/n)').lower()=='y':
+while input('\nNew game?(y)').lower()=='y':
+ d=['A',2,3,4,5,6,7,8,9,10]*4;h=[[],[]]
+ p=0;m()
+ p=1;r();m()
+ while t()in range(21)and input('More?(y)').lower()=='y':
   m()
  p=0
  while t()in range(16):
   m()
- p0=t();p=1;p1=t()
- if p0==-1 and p1!=-1 or p0<22 and(p1>21 or p1<p0 and p1!=-1):
-  r='Loss!'
- elif p0==p1 or p0>21 and p1>21:
-  r='Draw!'
- else:
-  r='Win!'
- print(r)
+ b=t();p=1;a=t()
+ print('Loss'if b>a else'Win'if a>b else'Draw')
--- Python/blackjack.py
+++ Python/blackjack.py
@@ -1,13 +1,12 @@
 from random import randint as rd
 
-name=('Banker','You')
-has=('Banker has','You have')
-faces=('J','Q','K')
-values=('A',2,3,4,5,6,7,8,9,10)+faces
-
-def pts(p):
+name = ('Banker','You')
+has = ('Banker has','You have')
+faces = ('J','Q','K')
+ 
+def pts():
   if len(hand[p]) == 2 and (hand[p][0][2:] == 'A' and hand[p][1][2:] in faces or hand[p][0][2:] in faces and hand[p][1][2:] == 'A'):
-    return -1
+    return 31
   points, a = 0, 0
   for card in hand[p]:
     if card[2:] == 'A':
@@ -19,36 +18,36 @@ def pts(p):
       points += int(card[2:])
   if points < 12 and a:
     points += 10
-  return points
+  return points if points < 22 else -1
 
 def deal():
-  hand[p].append(deck.pop(rd(0,len(deck)-1)))
-  print(f'{name[p]} got a {hand[p][-1]}.')
-  if pts(p) == -1:
+  card=deck.pop(rd(0,len(deck)-1))
+  hand[p]+=[card]
+  print(f"{name[p]} got {'an' if card[2:] == 'A' else 'a'} {card}.")
+  
+def more():
+  deal()
+  if pts() > 30:
     print(has[p],'Black Jack!')
-  elif pts(p)>21:
-    print(name[p],'busted')
+  elif pts() < 0:
+    print(name[p],'busted!')
   else:
-    print(f'{has[p]} {pts(p)}.')
-
+    print(f'{has[p]} {pts()}.')
+ 
 while input('\nNew game? (y/n) ').lower() == 'y':
-  deck = ['♠️'+str(i) for i in values] + ['♥️'+str(i) for i in values] + ['♣️'+str(i) for i in values] + ['♦️'+str(i) for i in values]
+  deck = [s+str(v) for v in ('A',2,3,4,5,6,7,8,9,10)+faces for s in ('♠️','♥️','♠️','♦️')]
   hand = [[],[]]
+  p = 0; more()
+  p = 1; deal(); more()
+  while pts() in range(21) and input('Would you like another card? (y/n) ').lower() == 'y':
+    more()
   p = 0
-  deal()
-  p = 1
-  hand[1].append(deck.pop(rd(0,len(deck)-1)))
-  print(f'{name[p]} got a {hand[p][-1]}.')
-  deal()
-  while pts(1) < 21 and input('Would you like another card? (y/n) ').lower() == 'y':
-    deal()
-  p=0
-  while pts(0) < 16 and pts(0) != -1:
-    deal()
-  p0, p1 = pts(0), pts(1)
-  if p0 == -1 and p1 != -1 or p0 < 22 and (p1 > 21 or p1 < p0 and p1 != -1):
+  while pts() in range(16):
+    more()
+  p0 = pts(); p = 1; p1 = pts()
+  if p0 > p1:
     print('You lost!')
-  elif p0 == p1 or p0 > 21 and p1 > 21:
-    print('Draw game!')
-  else:
+  elif p1 > p0:
     print('You won!')
+  else:
+    print('Draw game!')
