a753423f1dc49faf40f47b7ec82f55d7358076a0
locharp <lochardpatrick@outlook.com>
2023-02-17 21:31:22 +0200

    c

--- Dcoder/Medium/The_Game_of_Power.py
+++ Dcoder/Medium/The_Game_of_Power.py
@@ -1,6 +1,18 @@
-def s(n):
+def special(n):
+  if n in range(8):
+    return True if n in (1, 4) else False
+  for i in range(2, n//2):
+    j = i * i
+    while j <= n:
+      if j == n:
+        return True
+      j *= i
+  return False
+
+def nearest(n):
   for i in range(n):
-    
+    if special(n-i) or special(n+i):
+      return i
+
 for t in range(int(input())):
-  n = int(input())
-  for i in range(
+  print(nearest(int(input())))
