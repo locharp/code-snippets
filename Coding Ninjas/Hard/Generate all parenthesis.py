8408dc8eb2f2bb317a738f5e9a59678d3ae4266a
Lochard <lochardpatrick@outlook.com>
2023-08-23 18:39:13 +0300

    Generate all parenthesis.py

--- Coding Ninjas/Hard/Generate all parenthesis.py
+++ Coding Ninjas/Hard/Generate all parenthesis.py
@@ -8,7 +8,7 @@ for i in range( 2, n ):
     for j in range( 2, i // 2 + 1 ):
         for p in ans[ j - 1 ]:
             for q in ans[ i - j - 1 ]:
-                ans[ i - 1 ].add( p + q )
+                ans[ i - 1 ].update( ( p + q, q + p ) )
                 
 for p in ans[-1]:
     print( p )
