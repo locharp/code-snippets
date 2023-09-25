3a0bfbe51b52172477e215df6cdc6075e39fd02e
Lochard <110186560+locharp@users.noreply.github.com>
2023-09-25 06:16:07 +0300

    Create drink_about.py

---
+++ CodeWars/8kyu/drink_about.py
@@ -0,0 +1,9 @@
+def people_with_age_drink( age ):
+    if age > 20:
+        return "drink whisky"
+    elif age > 17:
+        return "drink beer"
+    elif age > 13:
+        return "drink coke"
+    else:
+        return "drink toddy"
