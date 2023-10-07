27abf02b2af93fb470694895cd3eb1ff97bb48df
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-07 14:24:14 +0300

    Create Zig Zag Subsequence.py

---
+++ Coding Ninjas/Hard/Zig Zag Subsequence.py
@@ -0,0 +1,17 @@
+def zigZagSubsequence( tokenArray, count = 1, i = 1, o = 0 ):
+    if i == len( tokenArray ):
+        return count
+    
+    if o > 0:
+        if tokenArray[ i - 1 ] > tokenArray[i]:
+            count += 1
+            o = -1
+    elif o < 0:
+        if tokenArray[ i - 1 ] < tokenArray[i]:
+            count += 1
+            o = 1
+    else:
+        count += 1
+        o = tokenArray[i] - tokenArray[ i - 1 ]
+        
+    return zigZagSubsequence( tokenArray, count, i + 1, o )
