12be1e36118c49589ec4e4b25faf065ac8eedb17
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-07 11:52:30 +0300

    Create Maximize It!.py

---
+++ HackerRank/Python/Maximize It!.py
@@ -0,0 +1,9 @@
+from itertools import product
+
+def sqaure( s ):
+    return pow( int( s ), 2 )
+    
+k, m = map( int, input().split() )
+x = [ list( map( square, input().split() ) ) for i in range( k ) ]
+ 
+print( max( sum( i ) % m for i in product( *x ) ) )
