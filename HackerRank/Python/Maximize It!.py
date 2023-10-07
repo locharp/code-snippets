f329f3f6ce326198e69a1e1afc517c0bdf62a8c9
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-07 13:25:08 +0300

    Update Maximize It!.py

--- HackerRank/Python/Maximize It!.py
+++ HackerRank/Python/Maximize It!.py
@@ -4,6 +4,6 @@ def sqaure( s ):
     return pow( int( s ), 2 )
     
 k, m = map( int, input().split() )
-x = [ list( map( square, input().split() ) ) for i in range( k ) ]
+x = [ list( map( square, input().split() ) )[ 1 : ] for i in range( k ) ]
  
 print( max( sum( i ) % m for i in product( *x ) ) )
