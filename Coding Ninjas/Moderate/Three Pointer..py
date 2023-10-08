0317aed1dd04b1321a2b467aca1d4dd32ffa023d
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-08 15:13:45 +0300

    Update Three Pointer..py

--- Coding Ninjas/Moderate/Three Pointer..py
+++ Coding Ninjas/Moderate/Three Pointer..py
@@ -1,16 +1,50 @@
-def threePointer( X:list, Y:list, Z:list, ans = 2147483647 ):
-    if len( X ) < 1 or len( Y ) < 1 or len( Z ) < 1:
-        return ans
+def threePointer( X:list, Y:list, Z:list ):
     
-    m = max( X[0], Y[0], Z[0] )
-    n = min( X[0], Y[0], Z[0] )
-    ans = min( ans, m - n )
+    ans = 2147483647
+    a, b, c = len( X ) - 1, len( Y ) - 1, len( Z ) - 1
+    i = j = k = 0
     
-    if X[0] == n:
-        X.pop( 0 )
-    elif Y[0] == n:
-        Y.pop( 0 )
-    else:
-        Z.pop( 0 )
-        
-    return threePointer( X, Y, Z, ans )
+    while len( X ) != i and len( Y ) != j and len( Z ) != k:
+    
+        if X[i] > Y[j]:
+            if X[i] > Z[k]:
+                if Y[j] > Z[k]:
+                    ans = min( ans, X[i] - Z[k] )
+                    
+                    if k < c:
+                        k += 1
+                    else:
+                        break
+                else:
+                    ans = min( ans, X[i] - Y[j] )
+                    
+                    if j < b:
+                        j += 1
+                    else:
+                        break
+            else:
+                ans = min( ans, Z[k] - Y[j] )
+                
+                if j < b:
+                    j += 1
+                else:
+                    break
+        elif X[i] > Z[k]:
+            ans = min( ans, Y[j] - Z[k] )
+            
+            if k < c:
+                k += 1
+            else:
+                break
+        else:
+            if Y[j] > Z[k]:
+                ans = min( ans, Y[j] - X[i] )
+            else:
+                ans = min( ans, Z[k] - X[i] )
+                
+            if i < a:
+                i += 1
+            else:
+                break
+                
+    return ans
