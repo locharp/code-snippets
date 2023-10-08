afa727f1cb705d2e99aca8c606d59a64098a6aa5
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-08 21:31:21 +0300

    Create Guido's Gorgeous Lasagna.py

---
+++ Exercism/Python/Guido's Gorgeous Lasagna.py
@@ -0,0 +1,14 @@
+EXPECTED_BAKE_TIME = 40
+
+def bake_time_remaining( minutes_in_oven ):
+    return EXPECTED_BAKE_TIME - minutes_in_oven
+
+
+
+def preparation_time_in_minutes( number_of_layers ):
+    return 2 * number_of_layers
+
+
+
+def elapsed_time_in_minutes( number_of_layers, elapsed_bake_time ):
+    return preparation_time_in_minutes( number_of_layers ) + elapsed_bake_time
