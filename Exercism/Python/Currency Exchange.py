ad6cbca3b50448cb8f2efb1a72bd2bf26ec86716
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-08 22:25:41 +0300

    Create Currency Exchange.py

---
+++ Exercism/Python/Currency Exchange.py
@@ -0,0 +1,37 @@
+def exchange_money( budget, exchange_rate ):
+    
+    return budget / exchange_rate
+
+
+
+def get_change( budget, exchanging_value ):
+    
+    return budget - exchanging_value
+
+
+
+def get_value_of_bills( denomination, number_of_bills ):
+    
+    return denomination * number_of_bills
+
+
+
+def get_number_of_bills( budget, denomination )
+
+    return budget // denomination 
+
+
+
+def get_leftover_of_bills( budget, denomination ):
+    
+    return budget % denomination
+
+
+
+def exchangeable_value( budget, exchange_rate, spread, denomination ):
+    exchange_rate_with_spread = exchange_rate * ( 1 + spread / 100 )
+    exchanged_value = exchange_money( budget, exchange_rate_with_spread )
+    number_of_bills = get_number_of_bills( exchanged_value, denomination )
+    receivable_value = get_value_of_bills( denomination, number_of_bills )
+
+    return receivable_value
