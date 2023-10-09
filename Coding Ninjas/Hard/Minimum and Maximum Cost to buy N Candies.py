96974630fa449153129ec9bb7448843ea0a2db4c
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-09 12:15:54 +0300

    Create Minimum and Maximum Cost to buy N Candies.py

---
+++ Coding Ninjas/Hard/Minimum and Maximum Cost to buy N Candies.py
@@ -0,0 +1,11 @@
+from math import ceil
+
+def minimumCost( cost, n, k ):
+    
+    return sum( sorted( cost )[ : ceil( n / ( k + 1 ) ) ] )
+
+
+
+def maximumCost( cost, n, k ):
+    
+    return sum( sorted( cost )[ -ceil( n / ( k + 1 ) ) ] )
