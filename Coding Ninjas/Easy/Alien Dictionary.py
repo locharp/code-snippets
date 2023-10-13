719b9765279663a83ec8d7a7d1fe8e791914e947
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-13 22:03:31 +0300

    Update Alien Dictionary.py

--- Coding Ninjas/Easy/Alien Dictionary.py
+++ Coding Ninjas/Easy/Alien Dictionary.py
@@ -3,6 +3,6 @@ def alienDictonary(n: int, words: List[str], order: str) -> bool:
     d = { j: i for i, j in enumerate( order ) }
     
     for i in range( n ):
-        words[i] = [ d[j] for j in words[i] ]
+        words[i] = [ d[j] if j in d else j for j in words[i] ]
         
     return words == sorted( words )
