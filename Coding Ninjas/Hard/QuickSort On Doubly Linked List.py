f56a45aa4bb42e36ff65461e0bffd514e78bff8b
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-13 16:55:38 +0300

    Create QuickSort On Doubly Linked List.py

---
+++ Coding Ninjas/Hard/QuickSort On Doubly Linked List.py
@@ -0,0 +1,42 @@
+def quick_sort( head ):
+    
+    prev = next = head
+    curr = head.next
+    
+    while curr is not None:
+        temp = curr.next
+        
+        if curr.value > head.value:
+            next.next = curr
+            curr.prev = next
+            next = curr
+            curr.next = None
+        else:
+            prev.prev = curr
+            curr.next = prev
+            prev = curr
+            curr.prev = None
+            
+        next.next = None
+        curr = temp
+        
+    if next is not head:
+        n = quick_sort( head.next )
+        n[0].prev = head
+        head.next = n[0]
+        next = n[1]
+        
+    if prev is not head:
+        head.prev.next = None
+        m = quick_sort( prev )
+        m[1].next = head
+        head.prev = m[1]
+        prev = m[0]
+        
+    return ( prev, next )  
+
+
+
+def quickSort( head ):
+    
+    return quick_sort( head )[0]
