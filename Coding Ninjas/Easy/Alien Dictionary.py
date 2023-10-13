97c48e80c35f46d73753aab540a0b6032a8dcf5a
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-13 17:24:05 +0300

    Create Alien Dictionary.py

---
+++ Coding Ninjas/Easy/Alien Dictionary.py
@@ -0,0 +1,8 @@
+def alienDictonary(n: int, words: List[str], order: str) -> bool:
+    
+    d = { j: i for i, j in enumerate( order ) }
+    
+    for i in range( n ):
+        words[i] = [ d[j] for j in words[i] ]
+        
+    return words == sorted( words )
