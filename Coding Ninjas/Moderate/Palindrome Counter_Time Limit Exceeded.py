7319691f9a44b158728701a4aea07e8a0ebed1b7
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-17 14:24:58 +0300

    Update Palindrome Counter_Time Limit Exceeded.py

--- Coding Ninjas/Moderate/Palindrome Counter_Time Limit Exceeded.py
+++ Coding Ninjas/Moderate/Palindrome Counter_Time Limit Exceeded.py
@@ -1,19 +1,21 @@
 def countPalindromeSubstrings( s: str ):
     n = len( s )
-    c = {}
+    c = set()
     d = set()
+    ans = 0
 
     for i in range( n ):
         for j in range( i + 1, n + 1 ):
             t = s[ i : j ]
             
             if t in c:
-                c[t] += 1
+                ans += 1
             elif t in d:
                 continue
             elif t == t[ : : -1 ]:
-                c[t] = 1
+                ans += 1
+                c.add( t )
             else:
                 d.add( t )
 
-    return sum( c.values() )
+    return ans
