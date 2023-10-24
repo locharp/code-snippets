92f316170e8cea944507d21abbed4bcae035534c
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-24 07:56:54 +0300

    Create Area of a Rectangle.py

---
+++ Coding Ninjas/Easy/Area of a Rectangle.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    
+    def __init__( self , length = 2, breadth = 10 ):
+
+        self.length = length
+        self.breadth = breadth
+
+
+
+    def getArea( self ):
+        
+        return self.length * self.breadth
