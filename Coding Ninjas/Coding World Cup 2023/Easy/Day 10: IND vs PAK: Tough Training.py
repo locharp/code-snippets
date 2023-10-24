3264a5243a4da08724e02e994071c7edc69d7c76
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-24 07:05:52 +0300

    Update Day 10: IND vs PAK: Tough Training.py

--- Coding Ninjas/Coding World Cup 2023/Easy/Day 10: IND vs PAK: Tough Training.py
+++ Coding Ninjas/Coding World Cup 2023/Easy/Day 10: IND vs PAK: Tough Training.py
@@ -1,6 +1,8 @@
-def checkSequence( a: int, d: int, x: int ) -> int:
+ef checkSequence( a: int, d: int, x: int ) -> int:
     
-    if d == 0:
+    if a > x and d > 0 or a < x and d < 0:
+        return 0
+    elif d == 0:
         return 1 if a == x else 0
-
+        
     return 1 if ( x - a ) % d == 0 else 0
