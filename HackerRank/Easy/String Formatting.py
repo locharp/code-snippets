4fda065ef4b82d1e43e0d7cce86acfff34c3b079
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-24 10:44:20 +0300

    Update String Formatting.py

--- HackerRank/Easy/String Formatting.py
+++ HackerRank/Easy/String Formatting.py
@@ -3,4 +3,4 @@ def print_formatted( number ):
     w = len( bin( number ) ) - 2
     
     for i in range( 1, number + 1 ):
-        print( f"{i:>{w}} {i:>{w}o} {i:>{w}X} {bin( i )[ 2 : ]:>{w}}" )
+        print( f"{i:>{w}} {i:>{w}o} {i:>{w}X} {i:>{w}b}" )
