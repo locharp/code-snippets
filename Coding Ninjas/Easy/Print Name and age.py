e7c06e86a4cb0786296d4a9e3c642c4085ff16cc
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-24 08:06:48 +0300

    Update Print Name and age.py

--- Coding Ninjas/Easy/Print Name and age.py
+++ Coding Ninjas/Easy/Print Name and age.py
@@ -2,3 +2,34 @@ name = input()
 age = input()
 
 print( "The name of the person is {} and the age is {}.".format( name, age ) )
+
+
+
+
+
+# OOP
+class Person:
+
+    def __init__( self, name, age ):
+
+        self.SetValue( name, age )
+
+
+
+    def SetValue( self, name, age ):
+
+        self.__name = name
+        self.__age = int( age )
+
+        
+
+    def GetValue( self ):
+        
+        print( "The name of the person is " + self.__name + " and the age is " + str( self.__age ) + "." )
+
+
+
+
+
+person = Person( input(), input() )
+person.GetValue()
