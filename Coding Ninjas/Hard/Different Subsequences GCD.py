530beca49fd77ab3a787820ac6af4f6f8743dfb9
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-26 03:48:20 +0300

    Update Different Subsequences GCD.py

--- Coding Ninjas/Hard/Different Subsequences GCD.py
+++ Coding Ninjas/Hard/Different Subsequences GCD.py
@@ -1,12 +1,39 @@
 from math import gcd
 
+def f( n ):
+
+    d = { 1, n }
+    j = n
+
+    for i in range( 2, n ):
+        j = n // i
+
+        if n % j == 0:
+            d.add( i )
+            d.add( j )
+
+        if i >= j:
+            break
+
+    return d
+
+
+    
 def differentGCDSubsequence( arr ):
     
-    a = tuple( set( arr ) )
+    a = sorted( set( arr ) )
     d = { i for i in a }
 
     for i, j in enumerate( a ):
+        s = f( j ) - d
+        
         for k in a[ i + 1 : ]:
-            d.add( gcd( j, k ) )
+            if len( s ) < 1:
+                break
+                
+            c = gcd( j, k )
+            d.add( c )
+
+            s.discard( c )
 
     return len( d )
