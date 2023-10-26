cb9a81fa6a03f6e976c007a014c04420baded844
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-26 03:27:21 +0300

    Create Product Of Array Except Self.py

---
+++ Coding Ninjas/Easy/Product Of Array Except Self.py
@@ -0,0 +1,13 @@
+def getProductArrayExceptSelf( arr, n ):
+    
+    p = [ 1 ] * n
+    q = [ 1 ] * n
+    m = 1000000007
+
+    for i in range( 1, n ):
+        p[i] = p[ i - 1 ] * arr[ i - 1 ] % m
+
+    for i in range( n - 1, 0, -1 ):
+        q[ i - 1 ] = 1[i] * arr[i] % m
+
+    return [ p[i] * q[i] % m for i in range( n ) ]
