6777d374d771c73663ca32067b2b34c18f153173
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-27 00:20:32 +0300

    Update Day 8: AUS vs SA: Performance Score.py

--- Coding Ninjas/Coding World Cup 2023/Moderate/Day 8: AUS vs SA: Performance Score.py
+++ Coding Ninjas/Coding World Cup 2023/Moderate/Day 8: AUS vs SA: Performance Score.py
@@ -1,17 +1,24 @@
 from typing import *
 
 def bitwise_inclusion( n: int, a: List[int] ) -> int:
-
+    
     ans = 0
 
     for i in range( n ):
         j = i + 1
         k = a[i]
-
-        while j < n and k > 0:
+        
+        while j < n:
             k &= a[j]
+
+            if k < 1:
+                break
+
             j += 1
 
         ans += ( j - i ) * a[i]
-    
+        
     return ans
+
+for i in range( int( input() ) ):
+    print( bitwise_inclusion( int( input() ), [ int( j ) for j in input().split() ] ))
