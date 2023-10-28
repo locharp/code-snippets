42f387af373f63f72bc08d08350f7e803294a1cf
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-28 16:49:39 +0300

    Update 841. Keys and Rooms.py

--- LeetCode/Medium/841. Keys and Rooms.py
+++ LeetCode/Medium/841. Keys and Rooms.py
@@ -3,15 +3,14 @@ class Solution:
     def canVisitAllRooms( self, rooms: List[ List[int] ] ) -> bool:
         
         s = { 0 }
-        t = { i for i in range( len( rooms ) ) }
+        t = { i for i in range( 1, len( rooms ) ) }
         
         while len( s ) > 0 and len( t ) > 0:
             o = s.pop()
             
-            if o in t:
-                t.remove( o )
-                
-                for i in rooms[o]:
+            for i in rooms[o]:
+                if i in t:
                     s.add( i )
-                    
+                    t.remove( i )
+            
         return len( t ) < 1
