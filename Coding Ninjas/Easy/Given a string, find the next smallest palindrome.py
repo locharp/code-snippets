dbae0467fda5bce95c550e8b7c5efc0cf881ee1e
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-30 19:10:48 +0200

    Update Given a string, find the next smallest palindrome.py

--- Coding Ninjas/Easy/Given a string, find the next smallest palindrome.py
+++ Coding Ninjas/Easy/Given a string, find the next smallest palindrome.py
@@ -2,27 +2,19 @@ def nextLargestPalindrome( s, length ):
     
     if length < 2:
         t = int( s )
-
-        if t < 9:
-            return str( t + 1 )
-        else:
-            return str( t + 2 )
-
+        
+        return str( t + ( 2 if t > 8 else 1 ) )
+        
     g = length % 2
     h = length // 2
     
-    if g > 0:
-        if s[ h - 1 : : -1 ] > s[ -h : ]:
-            return s[ : h ] + s[ h : : -1 ]
-    elif s[ h - 1 : : -1 ] > s[ -h : ]:
-        return s[ : h ] + s[ h - 1 : : -1 ]
+    if s[ h - 1 : : -1 ] > s[ -h : ]:
+        return s[ : h ] + s[ h if g > 0 else h - 1 : : -1 ]
 
     h += g
     t = str( int( s[ : h ] ) + 1 )
 
     if len( t ) > h:
-        return t[ : h ] + ( "0" if g < 1 else "" ) + t[ : h ][ : : - 1]
-    elif g > 0:
-        return t + t[ : -1 ][ : : -1 ]
+        return t[ : -g - 1 ] + t[ : : - 1]
     else:
-        return t + t[ : : -1 ]
+        return t + ( t[ h - 2 : : -1 ] if g > 0 else t[ : : -1 ] )
