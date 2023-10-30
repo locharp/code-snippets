d8f580847b612a41fdacc9a997210b1435d7bf00
Lochard <110186560+locharp@users.noreply.github.com>
2023-10-30 20:35:17 +0200

    Create Program to validate IP address.py

---
+++ Coding Ninjas/Easy/Program to validate IP address.py
@@ -0,0 +1,17 @@
+def isValidIPv4( ipAddress ):
+
+    a = ipAddress.split( "." )
+    
+    if len( a ) != 4:
+        return False
+        
+    for i in a:
+        if not i.isdecimal() or ( len( i ) > 1 and i[0] == "0" ):
+            return False
+            
+        i = int( i )
+        
+        if i < 0 or i > 255:
+            return False
+             
+    return True
