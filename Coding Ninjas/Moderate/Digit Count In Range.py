216b4403d2d2493099de79f022d00acf637b7f5f
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-01 09:25:30 +0200

    Update Digit Count In Range.py

--- Coding Ninjas/Moderate/Digit Count In Range.py
+++ Coding Ninjas/Moderate/Digit Count In Range.py
@@ -1,13 +1,16 @@
-def f( a, n, s, t ):
+def f( n, s, t, a ):
     
     m = len( s ) - 1
     ans = 1 if s[-1] >= t else 0
-
+    
     for i in range( m ):
-        if s[i] > t and t > "0":
+        if s[i] > t:
             ans += 10 ** ( m - i )
         elif s[i] == t:
             ans += n % 10 ** ( m - i ) + 1
+            
+        if t < "1":
+            ans -= 10 ** ( m - i )
 
         ans += int( s[i] ) * a[ m - i ]
 
@@ -17,12 +20,12 @@ def f( a, n, s, t ):
 
 def digitCount(K, A, B):
 
-    r = str( A - 1 )
+    r = str( A )
     s = str( B )
     t = str( K )
     a = [ 0 ] * len( s )
 
     for i in range( len( s ) - 1 ):
         a[ i + 1 ] = a[i] * 10 + 10 ** i
-    
-    return f( a, B, s, t ) -f( a, A - 1, r, t )
+
+    return f( B, s, t, a ) - f( A, r, t, a ) + r.count( t )
