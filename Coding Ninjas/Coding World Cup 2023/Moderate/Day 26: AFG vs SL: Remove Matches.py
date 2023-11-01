8b4bcb949e754df7d9546d257fd7e10b618453c0
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-01 03:31:09 +0200

    Update Day 26: AFG vs SL: Remove Matches.py

--- Coding Ninjas/Coding World Cup 2023/Moderate/Day 26: AFG vs SL: Remove Matches.py
+++ Coding Ninjas/Coding World Cup 2023/Moderate/Day 26: AFG vs SL: Remove Matches.py
@@ -6,8 +6,8 @@ def countElements( x: int, a: List[int] ) -> int:
     i = 0
 
     while i < len( a ):
-        if d[i] < x:
-            d[i] += 1
+        if d[ a[i] ] < x:
+            d[ a[i] ] += 1
             i += 1
         else:
             break
