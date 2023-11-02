5cd365aa4aa6064781912b3d7b6ba21bbb8a4942
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-02 07:01:00 +0200

    Create Author And Books.py

---
+++ Coding Ninjas/Moderate/Author And Books.py
@@ -0,0 +1,14 @@
+from string import ascii_uppercase
+
+def arrangeAuthors(s):
+    
+    A = "A" + ascii_uppercase
+    a = []
+
+    for i in range( len( s ) ):
+        a.append( str( i + 1 ) + ". " + s[i][0] )
+
+        for j in range( 1, len( s[i] ) ):
+            a.append( A[j] + ". " + s[i][j] )
+
+    return a
