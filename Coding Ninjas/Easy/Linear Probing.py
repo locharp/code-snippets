9128a8114dbfbde0e446846eaaff5d2817f1696e
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-03 04:06:41 +0200

    Create Linear Probing.py

---
+++ Coding Ninjas/Easy/Linear Probing.py
@@ -0,0 +1,25 @@
+def linearProbing( keys ):
+    
+    n = len( keys )
+    ans = [ -1 ] * n
+    
+    for i in keys:
+        j = i % n
+        if ans[j] < 0:
+            ans[j] = i
+        else:
+            t = True
+
+            for k in range( j + 1, n ):
+                if ans[k] < 0:
+                    ans[k] = i
+                    t = False
+                    break
+
+            if t:
+                for k in range( j ):
+                    if ans[k] < 0:
+                        ans[k] = i
+                        break
+
+    return ans
