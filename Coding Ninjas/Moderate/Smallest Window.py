b611e30540f2943f2d1be9d58ec1ee7504bd07b9
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-03 03:40:10 +0200

    Update Smallest Window.py

--- Coding Ninjas/Moderate/Smallest Window.py
+++ Coding Ninjas/Moderate/Smallest Window.py
@@ -2,29 +2,49 @@ from collections import *
 
 def smallestWindow( s, x ):
     
-    a = []
     c = Counter( x )
-    d = { i: [] for i in c }
-    m = u = v = 2147483647
-    n = len( x )
-    
-    for i, j in enumerate( s ):
-        if j in c:
-            if c[j] > 0:
-                n -= 1
-                c[j] -= 1
-            else:
-                a.remove( d[j].pop( 0 ) )
+    d = defaultdict( list )
+    u = v = 0
+    t = len( x ) - 1
 
-            d[j].append( i )
-            a.append( i )
+    while s[u] not in c:
+        u += 1
 
-            if n < 1:
-                o = a[-1] - a[0]
+    v = u
+    c[ s[u] ] -= 1
+
+    while t > 0:
+        v += 1
+
+        if s[v] in c:
+            if c[ s[v] ] > 0:
+                t -= 1
                 
-                if o < m:
-                    m = o
-                    u = a[0]
-                    v = a[-1]
-                    
-    return s[ u : v + 1 ]
+            c[ s[v] ] -= 1
+
+    while c[ s[u] ] < 0:
+        c[ s[u] ] += 1
+        u += 1
+        
+        while s[u] not in c:
+            u += 1
+
+    v -= u
+    t = u
+    
+    for i in range( u + v + 1, len( s ) ):
+        if s[i] in c:
+            c[ s[i] ] -= 1
+
+            while c[ s[t] ] < 0:
+                c[ s[t] ] += 1
+                t += 1
+                
+                while s[t] not in c:
+                    t += 1
+
+            if v > i - t:
+                u = t
+                v = i - t
+
+    return s[ u : u + v + 1 ]
