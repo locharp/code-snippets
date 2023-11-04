2da99521de48e94c4312c3801338862d3c81498d
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-04 16:07:40 +0200

    Create Olympic Medals.py

---
+++ Coding Ninjas/Moderate/Olympic Medals.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def f ( p, i, g, s, b ):
+
+    if g < 0 or s < 0 or b < 0:
+        return False
+    elif g == s == b == 0:
+        return True
+    
+
+    for j in range( i, len( p ) ):
+        if f( p, j + 1, g - p[j][0], s - p[j][1], b - p[j][2] ):
+            return True
+
+    return False
+
+
+
+def possibleOrNot( n: int, g: int, s: int, b: int, prizes: List[ List[int] ] ) -> int:
+
+    return 1 if f( prizes, 0, g, s, b ) else 0
