42a98169bf8744b516f7be68c733d6a7c993b5c7
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-04 16:08:03 +0200

    Update Olympic Medals.py

--- Coding Ninjas/Moderate/Olympic Medals.py
+++ Coding Ninjas/Moderate/Olympic Medals.py
@@ -7,7 +7,6 @@ def f ( p, i, g, s, b ):
     elif g == s == b == 0:
         return True
     
-
     for j in range( i, len( p ) ):
         if f( p, j + 1, g - p[j][0], s - p[j][1], b - p[j][2] ):
             return True
