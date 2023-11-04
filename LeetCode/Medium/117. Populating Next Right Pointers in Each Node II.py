1688e726edbbccc15be759541aade9f1de021427
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-04 17:35:16 +0200

    Create 117. Populating Next Right Pointers in Each Node II.py

---
+++ LeetCode/Medium/117. Populating Next Right Pointers in Each Node II.py
@@ -0,0 +1,31 @@
+class Solution:
+    
+    def __init__( self ):
+        
+        self.a = []
+        
+        
+        
+    def f( self, root, i ):
+        
+        if root is None:
+            return
+        
+        if i >= len( self.a ):
+            self.a.append( [] )
+            
+        self.a[i].append( root )
+        self.f( root.left, i + 1 )
+        self.f( root.right, i + 1 )
+        
+        
+        
+    def connect( self, root: 'Node' ) -> 'Node':
+        
+        self.f( root, 0 )
+        
+        for i in self.a:
+            for j in range( 1, len( i ) ):
+                i[ j - 1 ].next = i[j]
+                
+        return root
