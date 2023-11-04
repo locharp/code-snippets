dd27953431e7a2a3d0efc4da52720d80e396a034
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-04 05:18:33 +0200

    Update Next Greater Number.py

--- Coding Ninjas/Moderate/Next Greater Number.py
+++ Coding Ninjas/Moderate/Next Greater Number.py
@@ -1,18 +1,20 @@
-from sortedcontainers import SortedList
+from collections import defaultdict
+from sortedcontainers import SortedSet
 
 def nextGreater( S ):
 
-    a = SortedList()
+    d = defaultdict( int )
     n = len( S )
+    s = SortedSet()
 
     for i in range( n - 1, -1, -1 ):
-        for j in a:
+        d[ S[i] ] += 1
+        s.add( S[i] )
+
+        for j in s:
             if j > S[i]:
-                a.remove( j )
-                a.add( S[i] )
-
-                return S[ : i ] + j + "".join( a )
-
-        a.add( S[i] )
+                d[j] -= 1
+                
+                return S[ : i ] + j + "".join( [ k * d[k] for k in s ] )
 
     return "-1"
