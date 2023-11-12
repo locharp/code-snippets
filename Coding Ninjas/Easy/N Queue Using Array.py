81aeb18c233d6f3753b15c81c964e8f56584fcb8
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-12 07:35:46 +0200

    Create N Queue Using Array.py

---
+++ Coding Ninjas/Easy/N Queue Using Array.py
@@ -0,0 +1,31 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+class NQueue:
+    
+    def __init__( self, n, s ):
+        
+        self.q = [ [] for i in range( n + 1 ) ]
+        self.s = s
+        
+        
+        
+    def enqueue( self, x, m ):
+        
+        if len( self.q[m] ) < self.s:
+            self.q[m].append( x )
+            
+            return True
+        else:
+            return False
+            
+            
+            
+    def dequeue( self, m ):
+        
+        if len( self.q[m] ) > 0:
+            return self.q[m].pop( 0 )
+        else:
+            return -1
