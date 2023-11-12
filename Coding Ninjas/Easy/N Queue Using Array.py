6c0f7f21ad5cbf03c135f22a35295ea024053d73
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-12 08:38:58 +0200

    Update N Queue Using Array.py

--- Coding Ninjas/Easy/N Queue Using Array.py
+++ Coding Ninjas/Easy/N Queue Using Array.py
@@ -3,24 +3,28 @@ class NQueue:
     def __init__( self, n, s ):
         
         self.q = [ [] for i in range( n + 1 ) ]
-        self.s = s
+        self.m = s
+        self.n = 0
         
         
         
     def enqueue( self, x, m ):
         
-        if len( self.q[m] ) < self.s:
+        if self.n < self.m:
             self.q[m].append( x )
+            self.n += 1
             
             return True
         else:
             return False
-            
-            
-            
+        
+        
+        
     def dequeue( self, m ):
         
         if len( self.q[m] ) > 0:
+            self.n -= 1
+            
             return self.q[m].pop( 0 )
         else:
             return -1
