27aac1ae4519ae62a4fe1c62eefd4929d9ad8636
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-13 13:57:49 +0200

    Create Next smaller Palindrome.py

---
+++ Coding Ninjas/Easy/Next smaller Palindrome.py
@@ -0,0 +1,19 @@
+def nextSmallerPalindrome( s ):
+    
+    if s == "11":
+        return "9"
+    elif int( s ) < 11:
+        return str( int( s ) - 1 )
+        
+    n = len( s ) // 2
+    m = len( s ) % 2
+    
+    if s[ n - 1 : : -1 ] > s[ n + m : ]:
+        return s[ : n ] + s[ n - ( 1 - m ) : : -1 ]
+    else:
+        t = str( int( s[ : n + m ] ) - 1 )
+
+        if len( t ) < n:
+            return "9" * ( len( s ) - 1 )
+            
+        return t + t[ min( len( t ), n ) - m : : -1 ]
