6fe91ff22d09c855911ca94cc99ffe9b18a18afe
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-13 14:18:11 +0200

    Update Next smaller Palindrome.py

--- Coding Ninjas/Easy/Next smaller Palindrome.py
+++ Coding Ninjas/Easy/Next smaller Palindrome.py
@@ -8,12 +8,12 @@ def nextSmallerPalindrome( s ):
     n = len( s ) // 2
     m = len( s ) % 2
     
-    if s[ n - 1 : : -1 ] > s[ n + m : ]:
+    if s[ n - 1 : : -1 ] < s[ n + m : ]:
         return s[ : n ] + s[ n - ( 1 - m ) : : -1 ]
     else:
         t = str( int( s[ : n + m ] ) - 1 )
 
-        if len( t ) < n:
+        if len( t ) < n + m:
             return "9" * ( len( s ) - 1 )
             
-        return t + t[ min( len( t ), n ) - m : : -1 ]
+        return t + t[ n - m : : -1 ]
