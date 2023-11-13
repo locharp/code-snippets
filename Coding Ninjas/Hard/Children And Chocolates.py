f64f95602cdb0d1febddc15bc071649b53b0583b
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-13 15:48:35 +0200

    Update Children And Chocolates.py

--- Coding Ninjas/Hard/Children And Chocolates.py
+++ Coding Ninjas/Hard/Children And Chocolates.py
@@ -1,19 +1,20 @@
 from typing import *
 
-def f( ans, a, z, c, j ):
+def f( ans, a, c, j ):
     if c < 0:
-        print( -1, c, j )
         return -1
+    elif c == 0:
+        return ans
+
+    n = 0
 
     for i in range( j, len( a ) ):
-        if i < len( z ):
-            e = len( z[i] )
-        else:
-            e = 1
-
-        ans = max( ans, f( ans + e, a, z, c - a[i], i + 1 ) )
-    print( ans, c, j )
-    return ans
+        if c - a[i][1] < 0:
+            break
+            
+        n = max( n, f( a[i][1], a, c - a[i][0], i + 1 ) )
+        
+    return ans + n
 
 
 
@@ -24,32 +25,37 @@ def countChildren( n: int, friends: List[ List[int] ], m: int, chocolates: List[
 
     for i, j in friends:
         t = None
-
-        for w in z:
-            if i in w or j in w:
-                if t is None:
-                    t = w
-                    w.add( j )
-                    w.add( j )
-                else:
-                    t.update( w )
-                    continue
+        k = 0
         
+        while k < len( z ):
+            if i in z[k] or j in z[k]:
+                if t is None:
+                    t = z[k]
+                    z[k].add( i )
+                    z[k].add( j )
+                else:
+                    t.update( z[k] )
+                    z.pop( k )
+                    continue
+
+            k += 1
+
         if t is None:
             z.append( { i, j } )
 
-    a = [ 0 ] * len( z )
+    a = [ [ 0, len( z[i] ) ] for i in range( len( z ) ) ]
 
     for i, j in enumerate( chocolates ):
         t = True
 
         for k in range( len( z ) ):
             if i + 1 in z[k]:
-                a[k] += j
+                a[k][0] += j
                 t = False
         
         if t:
-            a.append( j )
-    print( z )
-    print( a )
-    return f( 0, a, z, c, 0 )
+            a.append( [ j, 1 ] )
+
+    a.sort()
+
+    return f( 0, a, c, 0 )
