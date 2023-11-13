1807101eaf88268b1aa169f28a65b2aaa7e5b97e
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-13 16:10:23 +0200

    Update Print All The Nodes At Distance K From The Given Node.py

--- Coding Ninjas/Moderate/Print All The Nodes At Distance K From The Given Node.py
+++ Coding Ninjas/Moderate/Print All The Nodes At Distance K From The Given Node.py
@@ -1,33 +1,37 @@
-def f( root, a, i ):
+from collections import defaultdict
+
+def f( root, d, p ):
     
     if root is None:
         return
-    elif i >= len( a ):
-        a.append( [] )
     
-    a[i].append( root.data )
-    f( root.left, a, i + 1 )
-    f( root.right, a, i + 1 )
+    d[p].append( root.data )
+    d[root.data].append( p )
+    f( root.left, d, root.data )
+    f( root.right, d, root.data )
     
 
 
 def kDistance( root, target, k ):
 
-    a = []
-    f( root.left, a, 0 )
-    a.reverse()
-    a.append( [ root.data ] )
-    f( root.right, a, len( a ) )
-    ans = []
+    if root is None:
+        return []
+        
+    d = defaultdict( list )
+    f( root.left, d, root.data )
+    f( root.right, d, root.data )
+    p = [ target ]
+    s = { target }
 
-    for i in range( len( a ) ):
-        if target in a[i]:
-            if i + k < len( a ):
-                ans += a[ i + k ]
-                
-            if i - k >= 0:
-                ans += a[ i - k ]
-                
-            break
+    for j in range( k ):
+        q = []
 
-    return ans
+        while len( p ) > 0:
+            for i in d[ p.pop() ]:
+                if i not in s:
+                    s.add( i )
+                    q.append( i )
+
+        p = q
+
+    return p
