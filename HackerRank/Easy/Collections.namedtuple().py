b3a350dcf8f20300d121b25f94029c6afa9473bf
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-14 20:50:14 +0200

    Update Collections.namedtuple().py

--- HackerRank/Easy/Collections.namedtuple().py
+++ HackerRank/Easy/Collections.namedtuple().py
@@ -1,6 +1,4 @@
 from collections import namedtuple
-
 n = int( input() )
 Student = namedtuple( "Student", input() )
-students = [ Student( *input().split() ) for i in range( n ) ]
-print( "{:.2f}".format( sum( int( student.MARKS ) for student in students ) / n ) )
+print( sum( int( Student( *input().split() ).MARKS ) for i in range( n ) ) / n )
