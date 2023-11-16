c7e841b6a9309db484ff0de44d5e123564d62937
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-16 15:15:14 +0200

    Create Convert Number To Words.py

---
+++ Coding Ninjas/Easy/Convert Number To Words.py
@@ -0,0 +1,38 @@
+def handleAll( n ):
+    
+    a = []
+    c = [ "", "one", "two", "three" ,"four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "", ]
+    d = [ "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ]
+    e = [ "", "", "hundred", "thousand", "", "lakh", "million", "", "hundred" ]
+    ans = []
+    
+    while n > 0:
+        a.append( n % 10 )
+        n //= 10
+        
+    for i in range( len( a ) ):
+        if e[i] != "":
+            ans.append( e[i] )
+        
+        if i % 3 == 0 and i + 1 < len( a ) and a[ i + 1 ] == 1:
+                ans.append( c[ a[ i + 1 ] * 10 + a[i] ] )
+                a[ i + 1 ] = 0
+        elif i % 3 == 1:
+            if d[ a[i] ] != "":
+                ans.append( d[ a[i] ] )
+        else:
+            if c[ a[i] ]:
+                ans.append( c[ a[i] ] )
+            
+        if i == 1 and len( ans ) > 0:
+            ans.append( "and" )
+            
+    i = 0
+    
+    while i < len( ans ):
+        if ans[i] in e and i + 1 < len( ans ) and ans[ i + 1 ] in e:
+            ans.pop( i )
+        else:
+            i += 1
+         
+    return " ".join( ans[ : : -1 ] )
