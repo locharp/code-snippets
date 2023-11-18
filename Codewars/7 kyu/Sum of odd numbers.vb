f6bdb4cd614818ee6d288ab0461a8f2bca3fe5b8
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-19 00:35:59 +0200

    Create Sum of odd numbers.vb

---
+++ Codewars/7 kyu/Sum of odd numbers.vb
@@ -0,0 +1,13 @@
+Public Module Kata
+    
+    
+    
+    Public Function RowSumOddNumbers( ByVal n As Integer ) As Integer
+        
+        Return n ^ 3
+        
+    End Function
+    
+    
+    
+End Module
