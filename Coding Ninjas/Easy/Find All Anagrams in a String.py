3a3b6b517178008c45dfa1088cbe046b3811bf7b
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-29 04:14:00 +0200

    Update Find All Anagrams in a String.py

--- Coding Ninjas/Easy/Find All Anagrams in a String.py
+++ Coding Ninjas/Easy/Find All Anagrams in a String.py
@@ -9,3 +9,32 @@ def findAnagramsIndices(str, ptr, n, m):
             ans.append( i )
             
     return ans
+
+
+
+# 2
+from collections import Counter
+
+def findAnagramsIndices( str, ptr, n, m ):
+    
+    c = Counter( ptr )
+    d = Counter( str[ : m ] )
+    ans = []
+    i = 0
+    
+    if c == d:
+        ans.append( i )
+        
+    for ch in str[ m : ]:
+        d[ str[i] ] -= 1
+        d[ch] += 1
+        
+        if d[ str[i] ] < 1:
+            del d[ str[i] ]
+        
+        i += 1
+        
+        if c == d:
+            ans.append( i )
+                        
+    return ans
