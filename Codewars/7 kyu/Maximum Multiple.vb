3140309510c21e47003321af19f608d96a53e29c
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-29 08:18:06 +0200

    Create Maximum Multiple.vb

---
+++ Codewars/7 kyu/Maximum Multiple.vb
@@ -0,0 +1,13 @@
+Public Module Kata
+
+
+
+    Public Function MaxMultiple( ByVal d As Integer, ByVal b As Integer ) As Integer
+    
+        Return ( b \ d ) * d
+        
+    End Function
+    
+    
+    
+End Module
