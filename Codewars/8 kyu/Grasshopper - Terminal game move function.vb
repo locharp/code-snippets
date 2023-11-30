ccedd1fb7bcc1fc6106ed6ee6791c15ce7d09201
Lochard <110186560+locharp@users.noreply.github.com>
2023-11-30 11:32:32 +0200

    Create Grasshopper - Terminal game move function.vb

---
+++ Codewars/8 kyu/Grasshopper - Terminal game move function.vb
@@ -0,0 +1,13 @@
+Public Module Kata
+    
+    
+    
+    Public Function Move( ByVal pos As Integer, ByVal roll As Integer ) As Integer
+        
+        Return pos + roll * 2
+        
+    End Function
+    
+    
+    
+End Module
