eff12f091bdd34a5be9c3eab11654ae5d072d9ea
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-03 07:03:47 +0200

    Create Century From Year.vb

---
+++ Codewars/8 kyu/Century From Year.vb
@@ -0,0 +1,13 @@
+Public Module Kata
+
+
+
+    Public Function Century( ByVal year As Integer ) As Integer
+    
+        return Math.Ceiling( year / 100 )
+        
+    End Function
+    
+    
+    
+End Module
