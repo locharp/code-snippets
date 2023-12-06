3ed001b6a104f0a593e015bb5531111b8a79e013
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-06 08:10:47 +0200

    Update Best Time to Buy and Sell Stock.py

--- Coding Ninjas/Moderate/Best Time to Buy and Sell Stock.py
+++ Coding Ninjas/Moderate/Best Time to Buy and Sell Stock.py
@@ -9,3 +9,21 @@ def maximumProfit( prices ):
             curr = price
 
     return ans
+
+
+
+# 2
+def maximumProfit( prices ):
+    
+    a = sorted( ( j, i ) for i, j in enumerate( prices ) )
+    ans = 0
+
+    for i, j in enumerate( prices ):
+        while len( a ) > 0:
+            if a[-1][1] <= i:
+                a.pop()
+            else:
+                ans = max( ans, a[-1][0] - j )
+                break
+                
+    return ans
