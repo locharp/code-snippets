9497d504f39132472e0ad98012b42e873f771803
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-06 07:35:49 +0200

    Create The Celebrity Problem.py

---
+++ Coding Ninjas/Moderate/The Celebrity Problem.py
@@ -0,0 +1,27 @@
+from collections import *
+
+def findCelebrity( n, knows ):
+    u = { i for i in range( n ) }
+    c = defaultdict( set )
+    d = { i: { i } for i in range( n ) }
+
+    for i in range( n ):
+        for j in range( n ):
+            if i == j:
+                continue
+
+            if knows( i, j ):
+                c[i].add( j )
+                d[j].add( i )
+
+    for i in u - set( c ):
+        for j in u - d[i]:
+            if knows( j, i ):
+                d[i].add( j )
+            else:
+                break
+
+        if len( d[i] ) == n:
+            return i
+
+    return -1
