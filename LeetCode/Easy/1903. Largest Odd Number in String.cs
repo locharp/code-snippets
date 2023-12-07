57cc821a1e42898927bc58594d88997dedc9e1ae
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-07 13:20:09 +0200

    Create 1903. Largest Odd Number in String.cs

---
+++ LeetCode/Easy/1903. Largest Odd Number in String.cs
@@ -0,0 +1,15 @@
+public class Solution
+{
+    public string LargestOddNumber( string num )
+    {
+        for ( int i = num.Length - 1; i >= 0; i-- )
+        {
+            if ( num[i] % 2 > 0 )
+            {
+                return num.Substring( 0, i + 1 );
+            }
+        }
+
+        return "";
+    }
+}
