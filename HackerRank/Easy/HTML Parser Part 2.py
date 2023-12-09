0f0d44861ce58e7aed00eeb305958498b72342d2
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-09 08:31:36 +0200

    Create HTML Parser Part 2.py

---
+++ HackerRank/Easy/HTML Parser Part 2.py
@@ -0,0 +1,22 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser( HTMLParser ):
+    
+    def handle_comment( self, data ):
+        
+        if "\n" in data:
+            print( ">>> Multi-line Comment" )
+        else:
+            print( ">>> Single-line Comment" )
+            
+        print( data )
+            
+            
+            
+    def handle_data( self, data ):
+        
+        if data.endswith( "\n" ):
+            return
+        
+        print( ">>> Data" )
+        print( data )
