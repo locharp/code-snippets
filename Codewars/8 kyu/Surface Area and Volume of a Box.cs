9f0fdc9615eb0f848dd024190abd30631e86fd4f
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-09 12:54:15 +0200

    Update Surface Area and Volume of a Box.cs

--- Codewars/8 kyu/Surface Area and Volume of a Box.cs
+++ Codewars/8 kyu/Surface Area and Volume of a Box.cs
@@ -2,9 +2,9 @@ using System;
 
 public class Kata
 {
-    public static int[] Get_size(int w,int h,int d)
+    public static int[] Get_size( int w,int h,int d )
     {
-        int area = w* 2 * h;
+        int area = w * 2 * h;
         area += h * 2 * d;
         area += d * 2 * w;
         int volume = w * h * d;
