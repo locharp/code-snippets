4f90bd2b76cc46c9b86e77af37f51d2ff28c2669
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-09 09:19:19 +0200

    Update Longest Mountain Subarray.py

--- Coding Ninjas/Easy/Longest Mountain Subarray.py
+++ Coding Ninjas/Easy/Longest Mountain Subarray.py
@@ -1,9 +1,40 @@
-from os import *
-from sys import *
-from collections import *
-from math import *
+def longestMountain( arr, n ):
+    
+    ans = i = j = 0
+    o = arr[0]
 
-def longestMountain(arr, n):
+    for k in arr[ 1 : ]:
+        if k > o:
+            if j > 0:
+                ans = max( ans, i + j )
+                j = 0
+                i = 2
+            elif i > 0:
+                i += 1
+            else:
+                i = 2
+        elif k < o:
+            if i > 0:
+                j += 1
+        else:
+            if j > 0:
+                ans = max( ans, i + j )
+                
+            i = j = 0
+
+        o = k
+        
+    if j > 0:
+        ans = max( ans, i + j )
+
+    return ans
+    
+
+
+
+
+# 2
+def longestMountain( arr, n ):
 
     i = m = 0
     j = 1
