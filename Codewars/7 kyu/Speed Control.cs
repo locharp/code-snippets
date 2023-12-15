6699c3558ccd92420fdb423ac8546c4aa9a0c6a9
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-15 11:17:15 +0200

    Update Speed Control.cs

--- Codewars/7 kyu/Speed Control.cs
+++ Codewars/7 kyu/Speed Control.cs
@@ -5,12 +5,10 @@ public class GpsSpeed
     public static int Gps( int s, double[] x )
     {
         double m = 0;
-        double n = 0;
-      
+        
         for ( int i = 1; i < x.Length; i++ )
         {
-            n = x[i] - x[ i - 1 ];
-            m = Math.Max( m, n );
+            m = Math.Max( m, x[i] - x[ i - 1 ] );
         }
         
         return ( int ) Math.Floor( m * 3600 / s );
