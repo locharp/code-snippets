6e5489d99db82a952c9997c7932078c15afac786
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-27 09:07:26 +0200

    Create 1578. Minimum Time to Make Rope Colorful.php

---
+++ LeetCode/Medium/1578. Minimum Time to Make Rope Colorful.php
@@ -0,0 +1,37 @@
+<?php
+
+
+
+class Solution
+{
+    function minCost( $colors, $neededTime )
+    {
+        $ans = 0;
+        $n = strlen( $colors );
+        
+        for ( $i = 0; $i < $n; )
+        {
+            if ( $colors[$i] != $colors[ $i - 1 ] )
+            {
+                $i++;continue;
+            }
+            
+            $a = array( $neededTime[ $i - 1 ] );
+            
+            while ( $i < $n && 
+                   $colors[ $i ] == $colors[ $i - 1 ] )
+            {
+                array_push( $a, $neededTime[$i++] );
+            }
+            
+            rsort( $a );
+            $ans += array_sum( array_slice( $a, 1 ) );
+        }
+        
+        return $ans;
+    }
+}
+
+
+
+?>
