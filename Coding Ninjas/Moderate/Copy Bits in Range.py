202e14c7e41d55d046b1de44dad01311591e1157
Lochard <110186560+locharp@users.noreply.github.com>
2023-12-31 21:42:17 +0200

    Update Copy Bits in Range.py

--- Coding Ninjas/Moderate/Copy Bits in Range.py
+++ Coding Ninjas/Moderate/Copy Bits in Range.py
@@ -1,10 +1,10 @@
 def copyBitsInRange( a: int, b: int, x: int, y: int ) -> int:
-
+    
     p = list( bin( a )[ 2 : ] )
     o = len( p )
     q = list( bin( b )[ 2 : ].zfill( o ) )
 
-    for i in range( -x, max( -y, -o ) - 1, -1 ):
+    for i in range( -x, -min( o, y ) - 1, -1 ):
         if p[i] == "1":
             q[i] = "1"
 
