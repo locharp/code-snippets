a03090880df5a4ea9606dae756c8456fd1e8fce2
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-01 12:15:09 +0200

    Create Enumerable Magic #1 - True for All?.java

---
+++ Codewars/8 kyu/Enumerable Magic #1 - True for All?.java
@@ -0,0 +1,17 @@
+import java.util.function.IntPredicate;
+
+public class Solution
+{
+    public static boolean all( int[] list, IntPredicate predicate )
+    {
+        for ( int i : list )
+        {
+            if ( !predicate.test( i ) )
+            {
+                return false;
+            }
+        }
+      
+        return true;
+    }
+}
