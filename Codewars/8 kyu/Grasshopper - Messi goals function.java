8c7a67a19a6c87334930f770781eb552aea23e1c
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-08 19:27:52 +0200

    Create Grasshopper - Messi goals function.java

---
+++ Codewars/8 kyu/Grasshopper - Messi goals function.java
@@ -0,0 +1,7 @@
+public class Goals
+{
+    public static int goals( int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals )
+    {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
