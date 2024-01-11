4d39a2f617fc28191f8159ce966a081362eb6f5b
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-11 12:37:18 +0200

    Create Grasshopper - Check for factor.vb

---
+++ Codewars/8 kyu/Grasshopper - Check for factor.vb
@@ -0,0 +1,13 @@
+Public Module Kata
+    
+    
+    
+    Public Function CheckForFactor( ByVal base As Integer, ByVal factor As Integer ) As Boolean
+        
+        Return base Mod factor < 1
+        
+    End Function
+    
+    
+    
+End Module
