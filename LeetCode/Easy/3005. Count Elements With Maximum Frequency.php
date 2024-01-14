0242f86e5b013cb1c7d1b99f7b27da05e60dabd6
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-14 07:36:16 +0200

    Create 3005. Count Elements With Maximum Frequency.php

---
+++ LeetCode/Easy/3005. Count Elements With Maximum Frequency.php
@@ -0,0 +1,27 @@
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    
+    function maxFrequencyElements( $nums )
+    {
+        $a = array_count_values( $nums );
+        rsort( $a );
+        $ans = $m = $a[0];
+        
+        foreach ( array_slice( $a, 1 ) as $i )
+        {
+            if ( $i < $m )
+            {
+                break;
+            }
+            
+            $ans += $i;
+        }
+        
+        return $ans;
+    }
+}
