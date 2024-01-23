720c989cd1cd173c0f615795d4f9f44a380d3930
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-23 19:26:24 +0200

    Create Help the Fruit Guy.php

---
+++ Codewars/7 kyu/Help the Fruit Guy.php
@@ -0,0 +1,25 @@
+<?
+
+
+
+function removeRotten( $fruitBasket )
+{
+    if ( !isset( $fruitBasket ) )
+    {
+        return [];
+    }
+        
+    for ( $i = 0; $i < count( $fruitBasket ); $i++ )
+    {
+        if ( str_starts_with( $fruitBasket[$i], "rotten" ) )
+        {
+            $fruitBasket[$i] = substr_replace( $fruitBasket[$i], strtolower( $fruitBasket[$i][6] ), 0, 7 );
+        }
+    }
+    
+    return $fruitBasket;
+}
+
+
+
+?>
