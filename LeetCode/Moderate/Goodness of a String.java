d9977184615c1fbca24cc0e90c1467db9bd5f791
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-27 21:45:36 +0200

    Create Goodness of a String.java

---
+++ LeetCode/Moderate/Goodness of a String.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.Character;
+
+public class Solution
+{
+	
+	public static int goodnessOfString( String s )
+	{
+		int ans = 0;
+		int k = 0;
+		int MOD = 1000000007;
+
+		for ( int i = 0; i < s.length(); i++ )
+		{
+			if ( Character.isDigit( s.charAt( i ) ) )
+			{
+				long j = 0;
+
+				while ( Character.isDigit( s.charAt( i ) ) )
+				{
+					j = ( j * 10 ) + ( Character.getNumericValue( s.charAt( i++ ) ) );
+				}
+				
+				j = ( j * k ) % MOD;
+				ans = ( ans + ( int ) j ) % MOD;
+				i--;
+			}
+			else if ( s.charAt( i ) == '[' )
+			{
+				k++;
+			}
+			else if ( s.charAt( i ) == ']' )
+			{
+				k--;
+			}
+		}
+
+		return ans;
+	}
+
+}
