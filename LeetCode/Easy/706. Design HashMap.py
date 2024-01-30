429b8e616cf305936a0bed4b788615734c364e07
Lochard <110186560+locharp@users.noreply.github.com>
2024-01-30 23:08:13 +0200

    Update 706. Design HashMap.py

--- LeetCode/Easy/706. Design HashMap.py
+++ LeetCode/Easy/706. Design HashMap.py
@@ -1,19 +1,23 @@
 class MyHashMap:
-
+    
     def __init__( self ):
-        self.map = [ -1 ] * 107
-
-
-
+        
+        self.map = [ -1 ] * 1000001
+        
+        
+        
     def put( self, key: int, value: int ) -> None:
+        
         self.map[key] = value
-
-
-
+        
+        
+        
     def get( self, key: int ) -> int:
+        
         return self.map[key]
-
-
-
+        
+        
+        
     def remove( self, key: int ) -> None:
+        
         self.map[key] = -1
