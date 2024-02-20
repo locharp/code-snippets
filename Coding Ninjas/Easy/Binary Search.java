d7005a4d71ab9edf9cc735650d90e59e50abd7c4
Lochard <110186560+locharp@users.noreply.github.com>
2024-02-20 03:43:16 +0000

    Create Binary Search.java

---
+++ Coding Ninjas/Easy/Binary Search.java
@@ -0,0 +1,31 @@
+public class Solution
+{
+
+    public static int search
+    ( int []nums, int target )
+    {
+        int i = 0;
+        int j = nums.length - 1;
+
+        while ( i <= j )
+        {
+            int k = ( i + j ) / 2;
+
+            if ( nums[k] == target )
+            {
+                return k;
+            }
+            else if ( nums[k] > target )
+            {
+                j = k - 1;
+            }
+            else
+            {
+                i = k + 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
