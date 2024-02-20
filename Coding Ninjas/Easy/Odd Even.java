e1ec5aad512bbba8ae747d9f753be4bdd4152624
Lochard <110186560+locharp@users.noreply.github.com>
2024-02-20 07:18:05 +0000

    Create Odd Even.java

---
+++ Coding Ninjas/Easy/Odd Even.java
@@ -0,0 +1,17 @@
+public class Solution
+{
+
+    public static String oddEven
+    ( int N )
+    {
+        if ( N % 2 > 0 )
+        {
+            return "odd";
+        }
+        else
+        {
+            return "even";
+        }
+    }
+
+}
