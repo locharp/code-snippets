36ed22c3c153102b96a81974f6faf78fe36be5ed
Lochard <110186560+locharp@users.noreply.github.com>
2024-02-26 23:48:34 +0000

    Update Fractional Knapsack.java

--- Coding Ninjas/Easy/Fractional Knapsack.java
+++ Coding Ninjas/Easy/Fractional Knapsack.java
@@ -4,32 +4,32 @@ public class Solution
 {
 
     public static double maximumValue
-	( Pair[] items, int n, int w )
-	{
-		double ans = 0;
-		Arrays.sort( items, ( x, y ) ->
-			Double.compare( ( double ) y.value / y.weight, ( double ) x.value / x.weight ) );
-		
-		for ( Pair item : items )
-		{
-			if ( w < item.weight )
-			{
-				ans += ( double ) item.value / item.weight * w;
-				w = 0;
-			}
-			else
-			{
-				ans += item.value;
-				w -= item.weight;
-			}
+    ( Pair[] items, int n, int w )
+    {
+        double ans = 0;
+        Arrays.sort( items, ( x, y ) ->
+            Double.compare( ( double ) y.value / y.weight, ( double ) x.value / x.weight ) );
+        
+        for ( Pair item : items )
+        {
+            if ( w < item.weight )
+            {
+                ans += ( double ) item.value / item.weight * w;
+                w = 0;
+            }
+            else
+            {
+                ans += item.value;
+                w -= item.weight;
+            }
 
-			if ( w < 1 )
-			{
-				break;
-			}
-		}
-		
-		return ans;
+            if ( w < 1 )
+            {
+                break;
+            }
+        }
+        
+        return ans;
     }
 
 }
